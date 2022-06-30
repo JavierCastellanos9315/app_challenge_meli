4169acc1c39a502d5f18e27827fad97df83bc18d
Javier Castellanos <javiercast9315@gmail.com>
2022-06-30 00:49:20 -0500

    Se modifica clase Main

--- app/src/main/java/com/example/app_challenge_meli/MainActivity.kt
+++ app/src/main/java/com/example/app_challenge_meli/MainActivity.kt
@@ -1,11 +1,84 @@
 package com.example.app_challenge_meli
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.app_challenge_meli.adapters.ProductsAdapter
+import com.example.app_challenge_meli.databinding.ActivityMainBinding
+import com.example.app_challenge_meli.viewmodels.ProductViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val productViewModel: ProductViewModel by viewModels()
+
+    private val adapter = ProductsAdapter {
+        productViewModel.onProductClicked(it)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener(btnSearch)
+
+        binding.edtSearch.setOnEditorActionListener(edtSearchListener)
+
+        productViewModel.state.observe(this){ state ->
+            binding.progress.visibility = if (state.loading) View.VISIBLE else View.GONE
+            state.products?.let {
+                adapter.products = it
+                adapter.notifyDataSetChanged()
+                if(adapter.products.isEmpty()){
+                    binding.cardMsg.visibility = View.VISIBLE
+                    binding.recycler.visibility = View.GONE
+                } else {
+                    binding.recycler.visibility = View.VISIBLE
+                    binding.cardMsg.visibility = View.GONE
+                }
+            }
+            state.navigateTo?.let {
+                //TODO acción al hacer click en producto
+            }
+        }
+
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        productViewModel.setNavigateTo()
+    }
+
+    val edtSearchListener = TextView.OnEditorActionListener { v, actionId, event ->
+        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            binding.button.callOnClick()
+            return@OnEditorActionListener true
+        }
+        false
+    }
+
+    val btnSearch =  View.OnClickListener{ v->
+        binding.button.isEnabled = false
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        if(isNetworkAvailable(this)){
+            productViewModel.getProducts(binding.edtSearch.text.toString())
+            binding.recycler.adapter = adapter
+        } else {
+            Toast.makeText(applicationContext,"this is toast message", Toast.LENGTH_SHORT).show()
+        }
+        binding.button.isEnabled = true
+
+    }
+
+
 }
