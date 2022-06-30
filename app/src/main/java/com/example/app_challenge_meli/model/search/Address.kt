2003b1a1cafb8d18c5f7c24096debae2f277712f
Javier Castellanos <javiercast9315@gmail.com>
2022-06-30 00:45:17 -0500

    Se agrega modelo de busqueda

---
+++ app/src/main/java/com/example/app_challenge_meli/model/search/Address.kt
@@ -0,0 +1,8 @@
+package com.example.app_challenge_meli.model.search
+
+data class Address(
+    var city_id: String? = "",
+    var city_name: String? = "",
+    var state_id: String? = "",
+    var state_name: String? = ""
+)
